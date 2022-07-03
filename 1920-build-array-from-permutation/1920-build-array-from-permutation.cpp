dada5dac7d75fb65b26ca7df514dc256a6293f33
Pritish Khera <80779475+pritish-codes@users.noreply.github.com>
2022-07-03 23:55:29 +0530

    Time: 34 ms (13.68%), Space: 16.2 MB (80.51%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         int n=nums.size();
-        vector<int>ans(n,0);
+        vector<int>ans(n);
 
         for(int i=0;i<n;i++){
             ans[i]=nums[nums[i]];
