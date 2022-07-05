6cb74cb380f7d65830ddb0d09428d53841cb0259
Pritish Khera <80779475+pritish-codes@users.noreply.github.com>
2022-07-05 17:47:28 +0530

    Time: 11 ms (70.05%), Space: 15.2 MB (43.06%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+       
+        string a(s.size(),' ');
+        for(int i=0;i<s.size();i++){
+            a[indices[i]]=s[i];
+            
+
+            
+        }
+        return a;
+        
+    }
+};
